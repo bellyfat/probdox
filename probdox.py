3b5d8bfd2ffcab5d30af59fbd73b81acc8f8cf85
cknoll <CarstenKnoll@gmx.de>
2016-11-19 15:01:50 +0100

    first lines of code (argparse)

---
+++ probdox.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# The code in this file is in Python 3 syntax
+
+# created: 2016-11-19 14:43:45 by Carsten Knoll
+
+"""
+This file is part of probdox.
+
+probdox is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='control a probdox repository')
+    parser.add_argument('command', help='one of the following commands: push | pull | status')
+
+
+    args = parser.parse_args()
+    print(args.command)
+
+if __name__ == "__main__":
+    main()
